aae38b95c403f52da08acd14d1306ab0a22cb36f
yani pranay <33549205+yanipanay@users.noreply.github.com>
2023-10-11 00:18:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+const title = document.getElementById("sizes");
+
+
+window.addEventListener("resize", myFunction);
+
+function myFunction() {
+	title.innerText = `Width : ${window.innerWidth} and Height: ${window.innerHeight}`;
+}
